b94530df3fd827310d7f267dbbaeb2bfcb1e0d89
Consibic <boyangcheng123@gmail.com>
2017-04-01 16:53:41 -0400

    lol

--- Weighted_graph.h
+++ Weighted_graph.h
@@ -83,8 +83,8 @@ double Weighted_graph::distance( int m, int n ) const{
         illegal_argument ex;
         throw ex;
     }
-    int* modified = new int[vertex_num];
-    int current_count = 0;
+    //int* modified = new int[vertex_num];
+    //int current_count = 0;
     if(m == n) return 0.0;
     Leftist_heap<Weighted_graph_vertex> *heap = new Leftist_heap<Weighted_graph_vertex>();
     Weighted_graph_vertex start_vertex = graph[m];
@@ -105,24 +105,26 @@ double Weighted_graph::distance( int m, int n ) const{
         ini_len = start_vertex.getCurrentEdge();
         start_index = start_vertex.getId();
         graph[start_index].setVisited(true);
-        modified[current_count] = start_index;
-        current_count += 1;
+        //modified[current_count] = start_index;
+        //current_count += 1;
         if(start_vertex.getId() == n){
-            for(int i = 0; i < current_count; i++){
-                graph[modified[i]].setVisited(false);
+            for(int i = 0; i < vertex_num; i++){
+                //graph[modified[i]].setVisited(false);
+                graph[i].setVisited(false);
             }
             heap->clear();
             delete heap;
-            delete [] modified;
+            //delete [] modified;
             return start_vertex.getCurrentEdge();
         }
         if(heap->empty()){
-            for(int i = 0; i < current_count; i++){
-                graph[modified[i]].setVisited(false);
+            for(int i = 0; i < vertex_num; i++){
+                //graph[modified[i]].setVisited(false);
+                graph[i].setVisited(false);
             }
             heap->clear();
             delete heap;
-            delete [] modified;
+            //delete [] modified;
             return INF;
         }
     }
