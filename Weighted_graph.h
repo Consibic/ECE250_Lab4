503fb9c9255645893058474539050c6ca78016c9
Consibic <boyangcheng123@gmail.com>
2017-04-01 17:43:50 -0400

    lol

--- Weighted_graph.h
+++ Weighted_graph.h
@@ -99,7 +99,7 @@ double Weighted_graph::distance( int m, int n ) const{
         ini_len = start_vertex.getCurrentEdge();
         start_index = start_vertex.getId();
         graph[start_index].setVisited(true);
-        //modified[current_count] = start_index;
+        modified[current_count] = start_index;
         //current_count += 1;
         for(int i = 0; i < vertex_num; i++){
             if(start_vertex.getEdge(i) != 0 && !graph[i].getVisited()){
